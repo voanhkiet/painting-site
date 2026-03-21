26c0b547fec9b3891f909412cf166ef36c63930d
voanhkiet <voanhkiet261192@gmail.com>
2026-03-21 16:24:56 +0700

    css

--- app.py
+++ app.py
@@ -253,8 +253,7 @@ Art Gallery
 
 # Create DB automatically
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+    
 
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
